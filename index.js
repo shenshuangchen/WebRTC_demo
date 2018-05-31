fe55bcff35545fef41f43578022f4164bbd470e9
shenshuangchen <shensc@brandeis.edu>
2018-05-31 16:06:05 +0800

    remove debug trace

--- index.js
+++ index.js
@@ -12,37 +12,16 @@ server.listen(port, function () {
 	console.log('服务器运行在localhost: %d', port)
 })
 
-//在线用户
-var onlineUsers = {};
-//当前在线人数
-var onlineCount = 0;
-var desc=null;
-
 io.sockets.on('connection', socket => {
 	var timestamp = Number(new Date());
-	console.log(timestamp,"这是第一次")
 	socket.on('offer', data => {
-		console.log("这是第三次");
 		socket.broadcast.emit('offer', data.sdp)
 	})
 	socket.on('candidate', data=>{
-		console.log('收到candidate')
 		socket.broadcast.emit('candidate', data.candidate)
 	})
 
 	socket.on('answer', data=>{
-		console.log("收到answer了", data.sdp)
 		socket.broadcast.emit('answer', data.sdp)
 	} )
-
-
-
 })
-
-
-// io.on('connect',socket=>{
-// 	socket.emit('name', {username: 'litingting ' + new Date()});
-// 	socket.on('myotherevent',function(data){
-// 		console.log(data + new Date());
-// 	})
-// })
